94250babbbf8e325c220434e78c1e41a8fb9e2ee
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-12 08:01:57 +0300

    fix: fix up function

--- src/navigation/up.js
+++ src/navigation/up.js
@@ -1,5 +1,15 @@
 import path from 'path';
 
-export const up = (currentPath) => {
-  return path.join(currentPath, '..');
+export const up = (args, currentPath) => {
+  try {
+
+    if (args.length !== 0 ) {
+      throw new Error('Сommand must be without parameters');
+    }
+    return path.join(currentPath, '..');
+
+  } catch (e) {
+    throw (e);
+  }
+
 }
