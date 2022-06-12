13d3346f3e9f4426b75bbcad7589114b06a3c687
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-12 08:01:39 +0300

    fix: fix ls function

--- src/navigation/ls.js
+++ src/navigation/ls.js
@@ -1,5 +1,17 @@
 import { readdir } from 'fs/promises';
 
-export const ls = async (currentPath) => {
-  return await readdir(currentPath);
+export const ls = async (args, currentPath) => {
+  try {
+
+    if (args.length !== 0 ) {
+      throw new Error('Сommand must be without parameters');
+    }
+
+    const list = await readdir(currentPath);
+    console.log(list);
+
+  } catch (e) {
+    throw (e);
+  }
+
 };
