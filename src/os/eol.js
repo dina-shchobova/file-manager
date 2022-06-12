51259d7d1a80dcf51085f6576c3da3b7c4d96d8c
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-12 05:48:43 +0300

    fix: remove unnecessary try catch

--- src/os/eol.js
+++ src/os/eol.js
@@ -2,12 +2,6 @@ import eol from "os";
 import { stdout } from 'process';
 
 export const getEOL = () => {
-
   const EOL = JSON.stringify(eol.EOL);
-
-  try {
-    stdout.write(`\nDefault system End-Of-Line = ${EOL}\n`);
-  } catch (e) {
-    throw (e);
-  }
+  stdout.write(`\nDefault system End-Of-Line = ${EOL}\n`);
 }
--- src/os/homedir.js
+++ src/os/homedir.js
@@ -2,12 +2,6 @@ import { homedir } from "os";
 import { stdout } from 'process';
 
 export const getHomedir = () => {
-
   const dir = homedir();
-
-  try {
-    stdout.write(`\nHome directory = ${dir}\n`);
-  } catch (e) {
-    throw (e);
-  }
+  stdout.write(`\nHome directory = ${dir}\n`);
 }
