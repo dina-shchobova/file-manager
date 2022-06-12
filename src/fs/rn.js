d16541e22677d8c1dcdd2a510994b92a44f875ee
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-12 07:59:50 +0300

    fix: fix rn function

--- src/fs/rn.js
+++ src/fs/rn.js
@@ -1,25 +1,20 @@
-import { access, rename, readdir } from 'fs/promises';
+import { access, rename } from 'fs/promises';
 import { dirname, sep } from 'path';
 import { stdout } from 'process';
+import { isExist } from "../utils/isExist.js";
 
 export const rn = async (args) => {
-
-  let [pathToFile, newName] = args;
-
   try {
+
+    let [pathToFile, ...newName] = args;
+    const newPath = dirname(pathToFile) + sep + newName.join(' ');
+
     await access(pathToFile);
-    // проверка того, что файл с новым именем уже существует
-    let newFileExists = false;
-    const files = await readdir(dirname(pathToFile));
-    for (const file of files) {
-      if (file === newName) {
-        newFileExists = true;
-      }
+    const isNewPathExist = await isExist(newPath);
+    if (isNewPathExist) {
+      throw new Error('File already exists');
     }
-    if (newFileExists === true) {
-      throw new Error('');
-    }
-    const newPath = dirname(pathToFile) + sep + newName;
+
     await rename(pathToFile, newPath);
     stdout.write('File has been renamed\n');
   } catch (e) {
