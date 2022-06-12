246ee84f7d0ebddb634341d6d39772584d93a9c8
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-12 14:30:36 +0300

    fix: fix add function

--- src/fs/add.js
+++ src/fs/add.js
@@ -3,7 +3,23 @@ import path from 'path';
 
 export const add = (args, currentDirectory) => {
   return new Promise((resolve, reject) => {
+
+    if (args.length !== 1 ) {
+      throw new Error('This command takes one parameter');
+    }
+
     let inputName = args.join(' ');
+
+    if (inputName.includes('\\') ||
+      inputName.includes('\/') ||
+      inputName.includes('\:') ||
+      inputName.includes('\*') ||
+      inputName.includes('\<') ||
+      inputName.includes('\>')
+    ) {
+      throw new Error('File name must not contain characters \\ \/ \: \* \< \>');
+    }
+
     const newFilePath = path.join(currentDirectory, `${inputName}`);
     const writeStream = createWriteStream(newFilePath, {flags: 'wx'});
 
